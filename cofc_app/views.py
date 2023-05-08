7d2ddc0ec4eb5ae94f6b5d244d7a96062db42b21
unk-Sekiguchi <show79jp@gmail.com>
2023-05-09 02:25:56 +0900

    add cofc input Form

--- cofc_app/views.py
+++ cofc_app/views.py
@@ -27,11 +27,35 @@ def index(request):
             gender = form.cleaned_data['gender_sentence']
             jobs = form.cleaned_data['jobs_sentence']
             other = form.cleaned_data['other_sentence']
-            sentence = f"以下特徴を持つクトゥルフ神話TRPGのキャラクターを生成して\n" \
-                        f"年齢:{age}\n" \
-                        f"性別:{gender}\n"\
-                        f"職業:{jobs}\n"\
-                        f"その他の特徴:{other}\n"
+            sentence = f"以下特徴を持つクトゥルフ神話TRPGのキャラクターとキャラクターの技能値を生成してください。\n"\
+                f"なお、技能値はクトゥルフ神話TRPG第7版ルールにて作成してください。\n"\
+                f"----\n"\
+                f"年齢:{age}\n"\
+                f"性別:{gender}\n"\
+                f"職業:{jobs}\n"\
+                f"その他の特徴:{other}\n"\
+                f"----\n"\
+                f"結果は下記のように出力してください。\n\n" \
+                f"----\n" \
+                f"【特徴】\n" \
+                f"・職業技能: AAA (nn%),BBB(mm%)…etc\n" \
+                f"・趣味技能: NNN (nn%),MMM(mm%)…etc\n\n" \
+                f"【その他の特徴】\n"\
+                f"【名前】: 〇〇\n"\
+                f"【性格】:　△△\n"\
+                f"【信念】:　××\n"\
+                f"【交友関係】:　⬜︎⬜︎\n\n"\
+                f"【技能値】\n" \
+                f"・STR: EE\n"\
+                f"・CON: FF\n"\
+                f"・POW: GG\n"\
+                f"・DEX: HH\n"\
+                f"・APP: II\n"\
+                f"・SIZ: JJ\n"\
+                f"・INT: KK\n"\
+                f"・EDU: FF\n"\
+                f"----\n" \
+
             print(sentence)
             # TODO: APIキーのハードコーディングは避ける
             load_dotenv('../.env')
@@ -39,6 +63,7 @@ def index(request):
 
             # ChatGPT
             response = openai.ChatCompletion.create(
+                #model="gpt-4",
                 model="gpt-3.5-turbo",
                 messages=[
                     {
@@ -53,7 +78,7 @@ def index(request):
             )
 
             chat_results = response["choices"][0]["message"]["content"]
-
+            print(chat_results)
     else:
         form = ChatForm()
 
